3dad13735ed1396c93bed169eae9cf3d1d18d5e5
Ayobami7G <lifewithnannyjay@gmail.com>
2026-01-21 21:12:19 +0100

    Add files via upload

---
+++ student_result_management_system.py
@@ -0,0 +1,30 @@
+def calculate_grade (score):
+    if score >= 70:
+        return "A"
+        elif score >= 60:
+            return "B"
+            elif score >= 50:
+                return "C"
+                elif score >= 45:
+                    return "D"
+                    else:
+                    return "F" 
+
+def main ():
+    students = {}
+    name = input("Adetimehin Oluwaseun Ayobami")
+    matric no = input(25/17919)
+    score = int(input("96"))
+    grade = calculate_grade(score)
+
+    student name = {
+        "score" : 96,
+        "grade" : A
+    }
+    print("\n---student Resuly ---")
+    print("Name:" name)
+     print("Score:" score)
+      print("Grade:" grade)
+
+      if __name__ == "__main__":
+        main()
